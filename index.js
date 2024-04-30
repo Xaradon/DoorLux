cc6fa68369911e17cca5db09516d6ac88fa9b9f8
Tim <xaradon@d4o-ag.de>
2024-04-30 15:41:59 +0200

    updated package json

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 
-const { DoorLuxPlatform } = require('./DoorLuxPlatform'); // Importiere die DoorLuxPlatform aus dem entsprechenden CommonJS-Modul
-const packageJson = require('./package.json');
+async function main(homebridge) {
+    const { DoorLuxPlatform } = await import('./DoorLuxPlatform');
 
-function main(homebridge) {
-    DoorLuxPlatform.loadPlatform(homebridge, packageJson, 'doorlux', DoorLuxPlatform);
+    if (DoorLuxPlatform && DoorLuxPlatform.loadPlatform) {
+        DoorLuxPlatform.loadPlatform(homebridge, require('./package.json'), 'doorlux', DoorLuxPlatform);
+    } else {
+        console.error("Failed to load the DoorLuxPlatform or the 'loadPlatform' method is not defined.");
+    }
 }
 
-module.exports = main; // Exportiere die main-Funktion als Modul
+module.exports = main;
