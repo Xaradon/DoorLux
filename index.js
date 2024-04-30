a5c2e4bfaaded2ff23d4752935c062d04769053d
Tim <xaradon@d4o-ag.de>
2024-04-30 15:44:00 +0200

    updated package json

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 'use strict';
 
 async function main(homebridge) {
-    const { DoorLuxPlatform } = await import('./DoorLuxPlatform');
+    try {
+        // Stellen Sie sicher, dass Sie die vollständige Dateierweiterung angeben, wenn nötig
+        const { DoorLuxPlatform } = await import('./DoorLuxPlatform.js');
 
-    if (DoorLuxPlatform && DoorLuxPlatform.loadPlatform) {
-        DoorLuxPlatform.loadPlatform(homebridge, require('./package.json'), 'doorlux', DoorLuxPlatform);
-    } else {
-        console.error("Failed to load the DoorLuxPlatform or the 'loadPlatform' method is not defined.");
+        if (DoorLuxPlatform && DoorLuxPlatform.loadPlatform) {
+            DoorLuxPlatform.loadPlatform(homebridge, require('./package.json'), 'doorlux', DoorLuxPlatform);
+        } else {
+            console.error("Failed to load the DoorLuxPlatform or the 'loadPlatform' method is not defined.");
+        }
+    } catch (error) {
+        console.error("Error loading the DoorLuxPlatform:", error);
     }
 }
 
